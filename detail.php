4e4c9fea92d62440ddace77d3b6c0a79d61c5c2d
Berzerker1031 <100453345+Berzerker1031@users.noreply.github.com>
2025-02-02 19:11:32 -0500

    Add files via upload

---
+++ detail.php
@@ -0,0 +1,22 @@
+<?php
+    require_once("functions.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Pet Shelter</title>
+    </head>
+    <body>
+        <?php printNav("detail") ?>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </body>
+    
+</html>
