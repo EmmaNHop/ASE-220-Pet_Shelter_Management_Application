9555b5db1ead6eb292eec952061942f665a68c03
Danny <dannylpoff3@gmail.com>
2025-02-16 11:13:58 -0500

    added more pets kinda, I just changed some of their names and such. We now have 19 Pets in the file

--- assets/js/pets.js
+++ assets/js/pets.js
@@ -190,16 +190,16 @@ var json =
         "Bio": "Meet Sunny, an outgoing and talkative Cockatiel who loves to whistle and sing! Sunny was hand-raised and enjoys spending time with people. He loves sitting on shoulders, mimicking sounds, and nibbling on millet treats. Sunny is looking for a home where he will receive plenty of interaction and enrichment to keep his curious mind engaged."
     },
     {
-        "Name": "Koda",
+        "Name": "Doug",
         "Pic": "assets/images/husky.png",
         "Breed": "Siberian Husky",
         "Type": "Dog",
-        "Sex": "Female",
+        "Sex": "Male",
         "Color": "Black and White",
         "SpayedNeutured": "Yes",
         "Age": "3",
-        "ID": "76238",
-        "Microchip Number": "982000364789931",
+        "ID": "76239",
+        "Microchip Number": "982000364789932",
         "DatePosted": "2024-01-15",
         "status": [
             "Health checked", 
@@ -207,19 +207,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Koda, a high-energy and intelligent Siberian Husky who loves adventure! Koda was surrendered by her previous owner due to her need for an active lifestyle. She thrives on outdoor activities like running, hiking, and playing in the snow. Koda is very vocal and enjoys 'talking' to her humans. She would do best with an experienced owner who can provide her with plenty of exercise and mental stimulation."
+        "Bio": "Meet Doug, a high-energy and intelligent Siberian Husky who loves adventure! Koda was surrendered by his previous owner due to his need for an active lifestyle. He thrives on outdoor activities like running, hiking, and playing in the snow. Doug is very vocal and enjoys 'talking' to his humans. He would do best with an experienced owner who can provide him with plenty of exercise and mental stimulation."
     },
     {
-        "Name": "Oliver",
+        "Name": "Olivia",
         "Pic": "assets/images/bp-lot-1-siamese-color-outdoor.png",
         "Breed": "Siamese",
         "Type": "Cat",
-        "Sex": "Male",
+        "Sex": "Female",
         "Color": "Seal Point",
         "SpayedNeutured": "Yes",
         "Age": "2",
-        "ID": "59821",
-        "Microchip Number": "982000364789642",
+        "ID": "59822",
+        "Microchip Number": "982000364789643",
         "DatePosted": "2024-02-01",
         "status": [
             "Health checked", 
@@ -227,19 +227,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Say hello to Oliver, a chatty and affectionate Siamese cat with striking blue eyes. Oliver loves to follow his humans around the house and 'talk' to them with his signature meows. He enjoys puzzle toys, warm sunspots, and cuddling under blankets. Oliver does best in a home where he gets plenty of attention and interaction."
+        "Bio": "Say hello to Olivia, a chatty and affectionate Siamese cat with striking blue eyes. Olivia loves to follow her humans around the house and 'talk' to them with her signature meows. She enjoys puzzle toys, warm sunspots, and cuddling under blankets. Olivia does best in a home where she gets plenty of attention and interaction."
     },
     {
-        "Name": "Peaches",
+        "Name": "Pear",
         "Pic": "./assets/images/pig.webp",
         "Breed": "Abyssinian",
         "Type": "Guinea Pig",
-        "Sex": "Female",
+        "Sex": "Male",
         "Color": "White and Brown",
         "SpayedNeutured": "Yes",
         "Age": "1",
-        "ID": "84532",
-        "Microchip Number": "982000364789765",
+        "ID": "84533",
+        "Microchip Number": "982000364789766",
         "DatePosted": "2024-01-25",
         "status": [
             "Health checked", 
@@ -247,19 +247,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Peaches, a playful and curious Abyssinian guinea pig with a big personality. Peaches loves to squeak for treats, zoom around her enclosure, and snuggle in soft bedding. She enjoys having a guinea pig friend to keep her company. Peaches would love a home where she gets plenty of fresh veggies and social interaction."
+        "Bio": "Meet Pear, a playful and curious Abyssinian guinea pig with a big personality. Pear loves to squeak for treats, zoom around his enclosure, and snuggle in soft bedding. He enjoys having a guinea pig friend to keep him company. Pear would love a home where he gets plenty of fresh veggies and social interaction."
     },
     {
-        "Name": "Bandit",
+        "Name": "Ferry",
         "Pic": "assets/images/ferret.jpg",
         "Breed": "Standard Ferret",
         "Type": "Ferret",
-        "Sex": "Male",
+        "Sex": "Female",
         "Color": "Sable",
         "SpayedNeutured": "Yes",
         "Age": "2",
-        "ID": "72365",
-        "Microchip Number": "982000364789384",
+        "ID": "72366",
+        "Microchip Number": "982000364789385",
         "DatePosted": "2024-01-10",
         "status": [
             "Health checked", 
@@ -267,19 +267,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Bandit, a mischievous and playful ferret with endless energy! Bandit was surrendered after his owner couldn’t keep up with his need for exploration and playtime. He loves tunneling, hiding his toys, and playing with other ferrets. Bandit is looking for a home with lots of enrichment and supervised playtime to keep him entertained."
+        "Bio": "Meet Ferry, a mischievous and playful ferret with endless energy! Ferry was surrendered after her owner couldn’t keep up with her need for exploration and playtime. She loves tunneling, hiding her toys, and playing with other ferrets. Ferry is looking for a home with lots of enrichment and supervised playtime to keep her entertained."
     },
     {
-        "Name": "Shelly",
+        "Name": "Shellbert",
         "Pic": "assets/images/turt.webp",
         "Breed": "Russian Tortoise",
         "Type": "Tortoise",
-        "Sex": "Female",
+        "Sex": "Male",
         "Color": "Brown and Yellow Shell",
         "SpayedNeutured": "N/A",
         "Age": "10",
-        "ID": "90241",
-        "Microchip Number": "982000364789987",
+        "ID": "90242",
+        "Microchip Number": "982000364789988",
         "DatePosted": "2024-01-05",
         "status": [
             "Health checked", 
@@ -287,19 +287,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Say hello to Shelly, a calm and easygoing Russian Tortoise who loves basking in the sun. Shelly was rehomed when her previous owner moved and couldn’t take her along. She enjoys munching on leafy greens and digging in her enclosure. With proper care, Shelly could live for many more years and would love a dedicated owner who understands her needs."
+        "Bio": "Say hello to Shellbert, a calm and easygoing Russian Tortoise who loves basking in the sun. Shellbert was rehomed when his previous owner moved and couldn’t take him along. He enjoys munching on leafy greens and digging in his enclosure. With proper care, Shellbert could live for many more years and would love a dedicated owner who understands his needs."
     },
     {
-        "Name": "Luna",
+        "Name": "Sol",
         "Pic": "assets/images/MainCoon.jpg",
         "Breed": "Maine Coon",
         "Type": "Cat",
-        "Sex": "Female",
+        "Sex": "Male",
         "Color": "Brown Tabby",
         "SpayedNeutured": "Yes",
         "Age": "3",
-        "ID": "45892",
-        "Microchip Number": "982000364789315",
+        "ID": "45893",
+        "Microchip Number": "982000364789316",
         "DatePosted": "2024-02-02",
         "status": [
             "Health checked", 
@@ -307,19 +307,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Say hello to Luna, a gentle and affectionate Maine Coon with a luxurious coat and a love for cuddles. Luna was found as a stray but quickly adapted to indoor life. She enjoys perching by the window, watching birds, and playing with feather toys. Luna is friendly with other cats and adores human companionship. She would thrive in a home where she gets plenty of attention and cozy spots to nap."
+        "Bio": "Say hello to Sol, a gentle and affectionate Maine Coon with a luxurious coat and a love for cuddles. Sol was found as a stray but quickly adapted to indoor life. She enjoys perching by the window, watching birds, and playing with feather toys. Sol is friendly with other cats and adores human companionship. He would thrive in a home where he gets plenty of attention and cozy spots to nap."
     },
     {
-        "Name": "Max",
+        "Name": "May",
         "Pic": "assets/images/goldRetriver.webp",
         "Breed": "Golden Retriever",
         "Type": "Dog",
-        "Sex": "Male",
+        "Sex": "Female",
         "Color": "Golden",
         "SpayedNeutured": "Yes",
         "Age": "4",
-        "ID": "57320",
-        "Microchip Number": "982000364789612",
+        "ID": "57321",
+        "Microchip Number": "982000364789613",
         "DatePosted": "2024-01-20",
         "status": [
             "Health checked", 
@@ -327,19 +327,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Max, a happy-go-lucky Golden Retriever with a heart full of love! Max was surrendered due to his previous owner’s relocation and is now looking for a forever home. He is incredibly friendly, energetic, and loves playing fetch. Max is great with kids and other pets, making him a fantastic addition to any family. His favorite activities include swimming, running in the park, and getting belly rubs."
+        "Bio": "Meet May, a happy-go-lucky Golden Retriever with a heart full of love! May was surrendered due to her previous owner’s relocation and is now looking for a forever home. She is incredibly friendly, energetic, and loves playing fetch. May is great with kids and other pets, making her a fantastic addition to any family. Her favorite activities include swimming, running in the park, and getting belly rubs."
     },
     {
-        "Name": "Daisy",
+        "Name": "Day",
         "Pic": "assets/images/lop.jpg",
         "Breed": "Holland Lop",
         "Type": "Rabbit",
-        "Sex": "Female",
+        "Sex": "Male",
         "Color": "White with Grey Spots",
         "SpayedNeutured": "Yes",
         "Age": "2",
-        "ID": "69241",
-        "Microchip Number": "982000364789827",
+        "ID": "69242",
+        "Microchip Number": "982000364789828",
         "DatePosted": "2024-01-28",
         "status": [
             "Health checked", 
@@ -347,19 +347,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Daisy, a sweet and gentle Holland Lop with the softest fur! Daisy was rescued from a backyard breeder and has since become a loving and playful companion. She enjoys munching on fresh greens, exploring tunnels, and getting gentle head scratches. Daisy is litter-trained and would love a home where she can roam and play safely."
+        "Bio": "Meet Day, a sweet and gentle Holland Lop with the softest fur! Day was rescued from a backyard breeder and has since become a loving and playful companion. He enjoys munching on fresh greens, exploring tunnels, and getting gentle head scratches. Day is litter-trained and would love a home where He can roam and play safely."
     },
     {
-        "Name": "Sunny",
+        "Name": "Cheese",
         "Pic": "assets/images/cockatiel.jpg",
         "Breed": "Cockatiel",
         "Type": "Bird",
-        "Sex": "Male",
+        "Sex": "Female",
         "Color": "Gray with Yellow Face",
         "SpayedNeutured": "N/A",
         "Age": "1",
-        "ID": "32187",
-        "Microchip Number": "982000364789529",
+        "ID": "32188",
+        "Microchip Number": "982000364789530",
         "DatePosted": "2024-01-18",
         "status": [
             "Health checked", 
@@ -367,19 +367,19 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Sunny, an outgoing and talkative Cockatiel who loves to whistle and sing! Sunny was hand-raised and enjoys spending time with people. He loves sitting on shoulders, mimicking sounds, and nibbling on millet treats. Sunny is looking for a home where he will receive plenty of interaction and enrichment to keep his curious mind engaged."
+        "Bio": "Meet Cheese, an outgoing and talkative Cockatiel who loves to whistle and sing! Cheese was hand-raised and enjoys spending time with people. She loves sitting on shoulders, mimicking sounds, and nibbling on millet treats. Cheese is looking for a home where he will receive plenty of interaction and enrichment to keep her curious mind engaged."
     },
     {
-        "Name": "Sunny",
+        "Name": "Sun",
         "Pic": "assets/images/cockatiel.jpg",
         "Breed": "Cockatiel",
         "Type": "Bird",
-        "Sex": "Male",
+        "Sex": "?",
         "Color": "Gray with Yellow Face",
         "SpayedNeutured": "N/A",
         "Age": "1",
-        "ID": "32187",
-        "Microchip Number": "982000364789529",
+        "ID": "32189",
+        "Microchip Number": "982000364789531",
         "DatePosted": "2024-01-18",
         "status": [
             "Health checked", 
@@ -387,7 +387,7 @@ var json =
             "Regularly dewormed",
             "Microchiped"
         ],
-        "Bio": "Meet Sunny, an outgoing and talkative Cockatiel who loves to whistle and sing! Sunny was hand-raised and enjoys spending time with people. He loves sitting on shoulders, mimicking sounds, and nibbling on millet treats. Sunny is looking for a home where he will receive plenty of interaction and enrichment to keep his curious mind engaged."
+        "Bio": "Meet Sun, an outgoing and talkative Cockatiel who loves to whistle and sing! Sun was hand-raised and enjoys spending time with people. They love sitting on shoulders, mimicking sounds, and nibbling on millet treats. Sun is looking for a home where they will receive plenty of interaction and enrichment to keep their curious mind engaged."
     }
 ];
 
